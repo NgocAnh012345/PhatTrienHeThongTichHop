03e72c6fb715e6d02f756134d2ae59d82e911ffc
NgocAnh012345 <72064588+NgocAnh012345@users.noreply.github.com>
2020-10-09 22:13:04 -0700

    Add files via upload

---
+++ ViewSource.java
@@ -0,0 +1,28 @@
+package lab3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ViewSource {
+	public static void main(String[] args) {
+		URL u;
+		String thisLine;
+		if (args.length > 0) {
+			try {
+				u = new URL("http://iuh.edu.vn");
+				try {
+					BufferedReader br = new BufferedReader(new InputStreamReader(u.openStream()));
+					while ((thisLine = br.readLine()) != null)
+						System.out.println(thisLine);
+				} catch (IOException e) {
+					System.err.println(e);
+				}
+			} catch (MalformedURLException e) {
+				System.err.println(e);
+			}
+		}
+	}
+}
